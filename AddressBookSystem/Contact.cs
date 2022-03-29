2568d0b2d6322c02ae2704525d16203f4d2ff00d
MdWasimAkram <wasimkhanktr@gmail.com>
2022-03-29 23:12:58 +0530

    [Wasim] Add: Add contact and exit

--- AddressBookSystem/Contact.cs
+++ AddressBookSystem/Contact.cs
@@ -10,12 +10,9 @@ namespace AddressBookSystem
     {
         public string firstName { get; set; }
         public string lastName { get; set; }
-
         public string address { get; set; }
         public string city { get; set; }
-
         public string state { get; set; }
-
         public string zipCode { get; set; }
         public string phoneNo { get; set; }
         public string email { get; set; }
